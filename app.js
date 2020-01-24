c64bb711a7709574d4620950d59812fba972422a
Ivo Krastev <52259854+BrascoBG@users.noreply.github.com>
2020-01-24 17:25:03 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function password(arg1){
+    let pass = arg1;
+
+    if (pass == 's3cr3t!P@ssw0rd'){
+        console.log('Correct!');
+    }
+    else {
+        console.log('Wrong Password!');
+    }
+}
+
+password('s3cr3t!P@ssw0rd');
